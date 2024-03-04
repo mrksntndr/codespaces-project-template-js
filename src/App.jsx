518ad344f5e46124fc17956aa9f96f5c693decc1
Mark Adrian Santander <89888470+mrksntndr@users.noreply.github.com>
2024-03-04 14:16:24 +0000

    first try

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Mark Adrian Santander",
+  title: "Computer Engineering Student",
+  email: "msdg2230@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
